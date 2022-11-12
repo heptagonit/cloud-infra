2e9ad3dd647ddafd23c7c872d3b5ac570f7cfd28
heptagonit <34228045+heptagonit@users.noreply.github.com>
2022-11-12 13:15:32 +0530

    Create vars.tf

---
+++ EC2/vars.tf
@@ -0,0 +1,16 @@
+variable "vpc_id" {}
+variable "name" {}
+variable "component" {}
+variable "region" {}
+variable "role_arn" {}
+variable "key_name" {}
+variable "image_id" {}
+variable "instance_type" {}
+variable "cpu_alarm_low_comparisonOperator"{} 
+variable "cpu_alarm_low_evaluation_periods"{} 
+variable "cpu_alarm_low_period"{} 
+variable "cpu_alarm_low_threshold"{}
+variable "cpu_alarm_high_comparisonOperator"{} 
+variable "cpu_alarm_high_evaluation_periods"{} 
+variable "cpu_alarm_high_period"{} 
+variable "cpu_alarm_high_threshold"{}
